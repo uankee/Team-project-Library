f9a656dfb35b68ec7cca73c8e16f52f0694e64da
zdkazuha <aartemchelyk@gmail.com>
2025-10-27 21:41:05 +0200

    Initialize Buisness Logic

--- Library-backend/Library/BusinessLogic/obj/Debug/net9.0/BusinessLogic.AssemblyInfo.cs
+++ Library-backend/Library/BusinessLogic/obj/Debug/net9.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5638c8696aa6fb9b982fa7d89cac165ddd97b470")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b13be29fea7718c7ef87e7b9643a77e4656099ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
