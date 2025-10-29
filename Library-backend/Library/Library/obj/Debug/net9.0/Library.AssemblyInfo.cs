5129a11ec2b7e52245f352ec14c17749eaf5eded
zdkazuha <aartemchelyk@gmail.com>
2025-10-29 19:53:30 +0200

    Create DTOs

--- Library-backend/Library/Library/obj/Debug/net9.0/Library.AssemblyInfo.cs
+++ Library-backend/Library/Library/obj/Debug/net9.0/Library.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02fbf781d177b268c8952d6cafcac9552b14c2d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d1c5077efc3582c40c7d99c31346cc37dc71f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
