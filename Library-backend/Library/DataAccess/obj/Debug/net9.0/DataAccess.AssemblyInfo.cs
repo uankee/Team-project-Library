95ff2dd0976707658b7290bf03257b32231c8e28
zdkazuha <aartemchelyk@gmail.com>
2025-10-29 18:11:25 +0200

    add-paginated

--- Library-backend/Library/DataAccess/obj/Debug/net9.0/DataAccess.AssemblyInfo.cs
+++ Library-backend/Library/DataAccess/obj/Debug/net9.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a656dfb35b68ec7cca73c8e16f52f0694e64da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda4f0e35dba21a098aa0329ae70a5b1be3c271d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
